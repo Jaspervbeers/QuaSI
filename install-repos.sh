7283e16aa242722f71b54453e89dbb1a23390dab
Jasper van Beers <j.j.vanbeers@tudelft.nl>
2025-02-25 14:52:38 +0100

    Added raw data unzip to install-repos.sh

--- install-repos.sh
+++ install-repos.sh
@@ -1,3 +1,23 @@
+echo "[ INFO ] Combining raw flight data zip files"
+cat data/raw.zip* > data/__ALL__.zip
+if [ $? -ne 0 ]
+then 
+    echo "[ ERROR ] Failed to combine zip files of raw data. You may need to extract these manually using e.g. 7zip: https://www.7-zip.org/"
+fi
+echo "[ INFO ] Extracting raw flight data"
+unzip -d data/ data/__ALL__.zip
+if [ $? -ne 0 ]
+then 
+    echo "[ ERROR ] Failed to extract .zip files of raw data. You need to do this manually using e.g. 7zip: https://www.7-zip.org/"
+fi
+rm data/__ALL__.zip
+if [ $? -ne 0 ]
+then 
+    echo "[ ERROR ] Failed to delete combined .zip file of raw data: __ALL__.zip, you may delete this file manually."
+fi
+
+echo "[ INFO ] Continuing with repository cloning..."
+
 echo '[ INFO ] Installing sysidpipeline from: https://github.com/Jaspervbeers/sysidpipeline.git'
 git clone git@github.com:Jaspervbeers/sysidpipeline.git
 if [ $? -ne 0 ]
